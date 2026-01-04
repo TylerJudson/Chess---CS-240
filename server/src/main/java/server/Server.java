8eb0b8356639416a5abe56a47cebdefd22ef952a
TylerJudson <TylerJudson23@gmail.com>
2026-01-04 10:34:53 -0700

    Registered teh endpoint for user management

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,26 @@
 package server;
 
+import handler.UserHandler;
 import io.javalin.*;
+import service.UserService;
 
 public class Server {
 
     private final Javalin javalin;
+    
+    private UserService userService;
+    private UserHandler userHandler;
 
     public Server() {
+        this.userService = new UserService();
+        this.userHandler = new UserHandler(userService);
+
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        // Register endpoints
+        javalin.post("/user", userHandler::handleRegistration);
+        javalin.post("/session", userHandler::handleLogin);
+        javalin.delete("/session", userHandler::handleLogout);
 
     }
 
