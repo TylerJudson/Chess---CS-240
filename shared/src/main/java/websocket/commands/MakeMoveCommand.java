383b70de00a119063b9de35cddbc676f8de941cf
TylerJudson <TylerJudson23@gmail.com>
2026-01-06 11:38:43 -0700

    Extended the gamecommands with the make move command

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,37 @@
+package websocket.commands;
+
+import chess.ChessMove;
+import java.util.Objects;
+
+public class MakeMoveCommand extends UserGameCommand {
+
+    private final ChessMove move;
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+        super(commandType, authToken, gameID);
+        this.move = move;
+    }
+    
+    public ChessMove getMove() {
+        return move;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MakeMoveCommand that)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        return Objects.equals(move, that.move);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), move);
+    }
+}
